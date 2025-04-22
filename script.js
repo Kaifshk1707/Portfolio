9910069c5691f400d59795b5584bcf30b55935a4
Asad Shaikh <shaikhasd20@gmail.com>
2025-04-22 23:24:18 +0530

    bugs fixed it.

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+const form = document.getElementById('contactForm');
+const popup = document.getElementById('popupMessage');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault(); // form submit
+
+    // Yahan form data ko backend pe bhejne ka code add kar sakte hain (optional)
+
+    popup.style.display = 'block';
+
+    setTimeout(() => {
+        popup.style.display = 'none';
+        form.reset();
+    }, 3000);
+});
