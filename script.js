d896a35a83f5e269d7a113ac6cc989690abf85ac
Asad Shaikh <shaikhasd20@gmail.com>
2025-04-22 22:47:27 +0530

    changes done.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function changeMessage() {
     document.getElementById("message").textContent = "You clicked the button!";
 }
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
